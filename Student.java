d10c8631b54d867f9e3595974d10743f7fba8513
Artemalatoo <150508652+Artemalatoo@users.noreply.github.com>
2024-10-27 15:35:44 +0600

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,21 +1,31 @@
 import java.util.*;
 public class Student {
-    String name;
-    int id;
-    String major;
-    int gpa;
-static void displayInfo(){
-    System.out.println("Study well");
-}
-public Student(String n, int i, String m, int g){
-    name=n;
-    id=i;
-    major=m;
-    gpa=g;
-}
-    public static void main(String[] args) {
-        Student s=new Student("Artem",230106020,"IT",4);
-        System.out.println(s.name+" "+s.id+" "+s.major+" "+s.gpa);
-        displayInfo();
+    private String name;
+    private int id;
+    private String major;
+    private double gpa;  
+    public Student(String name, int id, String major, double gpa) {
+        this.name = name;
+        this.id = id;
+        this.major = major;
+        this.gpa = gpa;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getMajor() {
+        return major;
+    }
+    public double getGpa() {
+        return gpa;
+    }
+    public static void displayInfo() {
+        System.out.println("Study well");
+    }
+    public void displayStudentInfo() {
+        System.out.println("Name: " + name + ", ID: " + id + ", Major: " + major + ", GPA: " + gpa);
     }
 }
