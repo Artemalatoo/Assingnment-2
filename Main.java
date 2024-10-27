3289ef08c91ea90b13cbe0c72852bb8c59fc4afb
Artemalatoo <150508652+Artemalatoo@users.noreply.github.com>
2024-10-27 15:36:21 +0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+
+class Main {
+    public static void main(String[] args) {
+        Student s = new Student("Artem", 230106020, "IT", 4.0);
+        s.displayStudentInfo();  
+        Student.displayInfo();    
+    }
+}
